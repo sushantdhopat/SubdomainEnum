865668afd9be62f8042abaaef96932c443cce7d3
sushant_dhopat <57283755+sushantdhopat@users.noreply.github.com>
2021-05-13 15:25:23 +0530

    Update subenum.sh

--- subenum.sh
+++ subenum.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+!/bin/bash
 #Author: sushant dhopat
 #Basic recon tool
 
 echo -e "\e[1;32m
+resolver=/home/sushant/subbrute/resolvers.txt
 target=$1
 mkdir new-$target
-
 #passive subdomain enumeration
 echo -e "\e[1;34m [+] Enumerating Subdomain from the assetfinder \e[0m"
 echo $target | assetfinder -subs-only| tee new-$target/$target-assetfinder.txt
@@ -13,16 +13,14 @@ echo -e "\e[1;34m [+] Enumerating Subdomain from the subfinder \e[0m"
 subfinder -d $target | tee new-$target/$target-subfinder.txt
 echo -e "\e[1;34m [+] Enumerating Subdomain from the amass \e[0m"
 amass enum -passive -d $target | tee new-$target/$target-amass.txt
-echo -e "\e[1;34m [+] Enumerating Subdomain from the sudomainizer \e[0m"
-python3 ~/SubDomainizer/SubDomainizer.py -u https://www.$target -o new-$target/$target-subdomainizer.txt
+echo -e "\e[1;34m [+] Enumerating Subdomain from the subbrute \e[0m"
+python /home/sushant/subbrute/subbrute.py $target -r $resolver -o new-$target/$target-subbrute.txt
 #copy above all different files finded subdomain in one spefic file
 cat new-$target/*.txt > new-$target/allsub-$target.txt
-rm new-$target/$target-assetfinder.txt new-$target/$target-subfinder.txt new-$target/$target-amass.txt new-$target/$target-subdomainizer.txt
-
+rm new-$target/$target-assetfinder.txt new-$target/$target-subfinder.txt new-$target/$target-amass.txt new-$target/$target-subbrute.txt
 #sorting the uniq domains 
 cat new-$target/allsub-$target.txt | sort -u | tee new-$target/allsortedsub-$target.txt
 rm new-$target/allsub-$target.txt
 echo -e "\e[1;34m [+] Running Httpx for live host \e[0m"
 cat new-$target/allsortedsub-$target.txt | httpx -silent | tee new-$target/validsubdomain-$target.txt
-
 echo -e "\e[1;34m [+] Finished all recon \e[0m"
